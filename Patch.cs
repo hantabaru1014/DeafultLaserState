1d26c7e9307c8cc867e12adec0785407452926b2
hantabaru1014 <hantabaru1014@gmail.com>
2024-01-14 23:47:32 +0900

    Set one hand at a time

--- Patch.cs
+++ Patch.cs
@@ -8,15 +8,21 @@ namespace Default_Laser_State
     {
         public override string Author => "LeCloutPanda";
         public override string Name => "Default Laser State";
-        public override string Version => "1.0.0";
+        public override string Version => "1.1.0";
 
-        public static ModConfiguration config;
-        [AutoRegisterConfigKey] public static ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("Default state", "What state will the lasers have", () => true);
+        [AutoRegisterConfigKey]
+        public static ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("Default state", "What state will the lasers have", () => true);
+        [AutoRegisterConfigKey]
+        public static ModConfigurationKey<bool> ENABLED_LEFT = new ModConfigurationKey<bool>("Default left state", "What state will the left laser have", () => true);
+        [AutoRegisterConfigKey]
+        public static ModConfigurationKey<bool> ENABLED_RIGHT = new ModConfigurationKey<bool>("Default right state", "What state will the right laser have", () => true);
+
+        public static ModConfiguration? config;
 
         public override void OnEngineInit()
         {
             config = GetConfiguration();
-            config.Save(true);
+            config!.Save(true);
 
             Harmony harmony = new Harmony("dev.lecloutpanda.defaultlaserstate");
             harmony.PatchAll();
@@ -33,7 +39,7 @@ namespace Default_Laser_State
                     if (__instance.Slot.ActiveUser != __instance.LocalUser)
                         return;
 
-                    ____laserEnabled.Value = config.GetValue(ENABLED);
+                    ____laserEnabled.Value = config!.GetValue(ENABLED) && __instance.Side == Chirality.Left ? config!.GetValue(ENABLED_LEFT) : config!.GetValue(ENABLED_RIGHT);
                 });
             }
         }
